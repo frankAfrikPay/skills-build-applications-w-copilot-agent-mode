0f2ebc031c5b818775711a4550ab1da0bedbfbd2
Paul Frank <p.nguiamba@it.afrikpay.com>
2025-12-08 10:20:43 +0000

    feat: update settings and urls for dynamic ALLOWED_HOSTS and api_root

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-ch-djo#4=4=800vbnf#lfmy$pgqo**%9vva6n5=j56dx*s=kpr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+codespace_host = f'{CODESPACE_NAME}-8000.app.github.dev' if CODESPACE_NAME else None
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if codespace_host:
+    ALLOWED_HOSTS.append(codespace_host)
 
 
 # Application definition
